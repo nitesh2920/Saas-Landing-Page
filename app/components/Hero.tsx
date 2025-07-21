f430654337128d3706033f2d1ff1fd36c337e7a2
nitesh2920 <niteshtiwari0234@gmail.com>
2025-07-21 22:22:50 +0530

    added hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import CursorImage from "../../public/images/cursor.png";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import MessageImage from "../../public/images/message.png";
+
+const Hero = () => {
+  return (
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip">
+      {/* gradient div */}
+      <div className="absolute h-[375px] w-[750px] rounded-[100%] bg-black left-1/2 -translate-x-1/2  border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-110px)] sm:top-[calc(100%-130px)] border sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[750px] "></div>
+
+      <div className="container relative">
+        <div className="flex justify-center mt-8  ">
+          <div className="inline-flex relative ">
+            <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center inline-flex">
+              Fancy UI <br /> is more
+            </h1>
+
+            <motion.div
+              initial={{ opacity: 0, y: 100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, ease: "easeOut" }}
+              viewport={{ once: true, amount: 0.5 }}
+              className="absolute  right-[420px] top-[108px] hidden sm:inline"
+              drag
+              dragSnapToOrigin
+            >
+              <Image
+                src={CursorImage}
+                alt="cursorImage"
+                height={200}
+                width={200}
+                className="max-w-none"
+                draggable="false"
+              />
+            </motion.div>
+
+            <motion.div
+              initial={{ opacity: 0, y: -100 }}
+              whileInView={{ opacity: 100, y: 0 }}
+              transition={{ duration: 0.6, ease: "easeOut" }}
+              viewport={{ once: false, amount: 0.5 }}
+              className="absolute left-[440px] top-[60px] hidden sm:inline  "
+              drag
+              dragSnapToOrigin
+            >
+              <Image
+                src={MessageImage}
+                alt="messageImage"
+                height={200}
+                width={200}
+                className="max-w-none"
+              />
+            </motion.div>
+          </div>
+        </div>
+
+        <div className="flex justify-center  mt-3 ">
+          <p className="text-xl text-center mt-9 max-w-md">
+            Experience a sleek, modern interface built for clarity and
+            performance. Designed to impress, crafted to deliver every pixel has
+            a purpose. Experience a sleek, modern interface built for clarity
+            and performance.
+          </p>
+        </div>
+
+        <div className="flex justify-center mt-9 ">
+          <button className="bg-white text-black py-3 px-5 rounded-lg font-medium">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
+
 
 
 export default function Home() {
   return (
    <>
    <Navbar />
+   <Hero/>
    </>
   );
 }
