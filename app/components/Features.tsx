abd732f3f254f29f3c537a6d243ce7f3070fac77
nitesh2920 <niteshtiwari0234@gmail.com>
2025-07-23 00:32:23 +0530

    added features section

---
+++ app/components/Features.tsx
@@ -0,0 +1,19 @@
+export default function Features() {
+  return (
+    <>
+      <div className="  bg-black text-white py-[70px] sm:py-[24px] border">
+        <div className="container">
+            <h2 className="text-center font-bold test-5xl sm:text-6xl tracking-tighter">Everything you need </h2>
+            <div className="max-w-xl mx-auto"
+            >
+            <p className="text-center mt-5 text-xl text-white/70"> 
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, odit in facilis mollitia dolorum rerum.
+
+            </p>
+            </div>
+        </div>
+        
+      </div>
+    </>
+  );
+}
