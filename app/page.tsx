bfc82dd2b7aae6ca40fa10ea581436c31c459d6c
nitesh2920 <niteshtiwari0234@gmail.com>
2025-07-23 00:43:05 +0530

    added product

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import LogoSection from './components/LogoSection';
+import Product from './components/Product';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
    <Navbar />
    <Hero/>
    <LogoSection/>
+   <Product/>
    </div>
     </>
    
