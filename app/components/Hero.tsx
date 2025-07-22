59734b4b6e384143c9e1c886f6e013e79c12f09a
nitesh2920 <niteshtiwari0234@gmail.com>
2025-07-22 13:29:17 +0530

    updated hero Section 1.1

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import MessageImage from "../../public/images/message.png";
 
+
 const Hero = () => {
   return (
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip">
@@ -52,6 +53,7 @@ const Hero = () => {
                 height={200}
                 width={200}
                 className="max-w-none"
+                draggable="false"
               />
             </motion.div>
           </div>
