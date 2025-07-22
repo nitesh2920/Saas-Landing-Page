0dac62112324d830dc4353851879243d4ee28beb
nitesh2920 <niteshtiwari0234@gmail.com>
2025-07-22 20:27:10 +0530

    adding linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +12,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules:{
+      "no-multiple-empty-lines":["error",{max:1, maxEOF: 0}],
+    },
+  }
 ];
 
 export default eslintConfig;
