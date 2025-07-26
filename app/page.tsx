b0ab48e7adcd33eb4e93b2be5ce9132455aa69c6
nitesh2920 <niteshtiwari0234@gmail.com>
2025-07-26 23:12:25 +0530

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import LogoSection from './components/LogoSection';
 import Product from './components/Product';
 import Faq from './components/FaqSection';
 import Features from './components/Features';
-import PricingCard from './components/PricingCard';
 import { PricingSection } from './components/PricingSection';
 
 export default function Home() {
